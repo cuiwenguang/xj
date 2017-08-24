ffe994b38c3ed4fadf6678393b2a58d8ddfa7cf0
yangling <235177791@qq.com>
2017-08-24 14:03:25 +0800

    修改接口和二维码

--- app/api.py
+++ app/api.py
@@ -1,12 +1,16 @@
+# -*- coding:utf-8 -*-
 import json
-from django.http.response import JsonResponse
+from django.http.response import JsonResponse, HttpResponse
 from django.contrib.auth import authenticate, login,logout
-
 from .qrcode import generate
+from app.models import PersonnelProfile
+from django.views.decorators.csrf import csrf_exempt
 
+@csrf_exempt
 def sign_in(request):
     '''用户登录接口'''
-    json_data = json.loads(request.body)
+
+    json_data = json.loads(request.body.decode())
     username = json_data.get("username", "")
     password = json_data.get("password", "")
     user = authenticate(request, username=username, password=password)
@@ -23,10 +27,10 @@ def sign_in(request):
             "msg": "未授权的用户",
             "data": None
         })
-
+@csrf_exempt
 def sign_out(request):
     '''用户注销'''
-    logout()
+    logout(request)
     return JsonResponse({
         "state": 200,
         "msg": "注销完成",
@@ -35,16 +39,16 @@ def sign_out(request):
 def qrcode_generate(request):
     '''测试生成二维码'''
     try:
-        data = {
-            "id": "uuid",
-            "name": "name",
-            "idcard": "idcard",
-            "photo": "photo"
-        }
-        generate(data)
-        return JsonResponse({"state": 200, "msg": "完成", "data": "/static/qrimage/"+data +".jpg"})
-    except:
-        return JsonResponse({"state": 500, "msg": "发生错误", "data": None})
-
-
+        pers = PersonnelProfile.objects.exclude(ID_number='').exclude(name='')[0:3]
+        for per in pers:
+            data = {
+                "uuid": per.personnel_uuid,
+                "name": per.name,
+                "ID_number": per.ID_number,
+                "ID_photo": per.ID_photo
+            }
+            path_file = generate(data)
+        return JsonResponse({"state": 200, "msg": "完成", "data": path_file})
+    except Exception as e:
+        return HttpResponse(e)
 
--- app/qrcode.py
+++ app/qrcode.py
@@ -1,21 +1,32 @@
+# -*- coding:utf-8 -*-
 import json
-import uuid
-import  qrcode
+import qrcode
+import os
 
-from xj.settings import QRCODE_PATH
-
-def generate(content):
+def generate(data):
     '''生成二维码的方法'''
-    file_name = '_'.join([content["name"], content["id"]])
-    path = ''.join([QRCODE_PATH, file_name, '.jpg'])
-    qr = qrcode.QRCode(
-        version=1,
-        error_correction=qrcode.constants.ERROR_CORRECT_L,
-        box_size=10,
-        border=4
-    )
-    img = qr.make(json.dumps(content))
-    img.save(path)
-    # 二维码路径保存回数据库
+    images_path = os.getcwd() + '/static/qrimage/'
+    file_name = '_'.join([data["name"], data["uuid"]]) + '.jpg'
+    try:
+        if not os.path.exists(images_path):
+            os.makedirs(images_path)
+        # destination = open(images_path + file_name, 'wb+')
+        # img = qrcode.make("some date here")
+        # img.save(destination, 'JPEG')
+        qr = qrcode.QRCode(
+            version=1,
+            error_correction=qrcode.constants.ERROR_CORRECT_L,
+            box_size=10,
+            border=4
+        )
+        content = json.dumps(data).encode("utf-8")
+        qr.add_data(content)
+        qr.make(fit=True)
+        img = qr.make_image()
+        img.save(images_path + file_name)
+        return file_name
+    except Exception as e:
+        print(e)
 
+    # 二维码路径保存回数据库
 
