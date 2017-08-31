d46691046c37c5136b1065b8a85de190cac7436f
yangling <235177791@qq.com>
2017-08-31 11:57:51 +0800

    updated

--- app/views.py
+++ app/views.py
@@ -1,6 +1,4 @@
-"""
-Definition of views.
-"""
+# -*- coding:utf-8 -*-
 from django.views.decorators.csrf import csrf_exempt
 from django.db.models import Q
 from django.shortcuts import render, redirect
